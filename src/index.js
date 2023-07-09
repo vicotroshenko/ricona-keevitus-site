0b2b81b7526792a2f24b5b1da02578756ac7e9df
vicotroshenko <otroshenko.vl@gmail.com>
2023-07-09 16:31:39 +0300

    add slider styles

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const openModalBtn = document.querySelectorAll('[data-modal-open]');
 const closeModalBtn = document.querySelector('[data-modal-close]');
 const modal = document.querySelector('[data-modal]');
 const imageContainer = document.querySelector('.js-container-slider');
-const btnBack = document.querySelector('.js-btn-slider-back');
-const btnForward = document.querySelector('.js-btn-slider-forward');
+const sliderRef = document.querySelector('.js-slider');
+const imgRef = document.querySelectorAll('.js-img');
 
 let imageList = [];
 let index = 0;
@@ -11,24 +11,34 @@ let index = 0;
 openModalBtn.forEach(btn => btn.addEventListener('click', toggleModal));
 openModalBtn.forEach(btn => btn.addEventListener('click', getImage));
 closeModalBtn.addEventListener('click', toggleModal);
-btnBack.addEventListener('click', getSlider);
-btnForward.addEventListener('click', getSlider);
 
+sliderRef?.addEventListener('click', getSlider);
+console.log(sliderRef)
 function toggleModal() {
   modal.classList.toggle('is-hidden');
 }
 
-openModalBtn.forEach(btn => imageList.push(btn.src));
+imgRef.forEach(img => imageList.push(img.src));
 console.log(imageList);
 
 function getImage(event) {
   imageContainer.innerHTML = `<img src="${event.target.src}" alt="" class="img"/>`;
 }
+
 function getSlider(event) {
   const name = event.target.name;
-  index = name === 'forward' ? index + 1 : index - 1;
+  if(name === undefined || name === "") {return};
+  // if(name === 'scale') {
+  //   console.log(imageContainer.style)
+  //   imageContainer.style.width = '200%;
+  //   imageContainer.style.height = '140vw';
+  //   return;
+  // }
+  index = name === 'forward' ? index + 1 : index;
+  index = name === 'back' ? index - 1 : index;
   index = index > imageList.length - 1 ? 0 : index;
   index = index < 0 ? imageList.length - 1 : index;
   console.log(index);
+
   imageContainer.innerHTML = `<img src="${imageList[index]}" alt="" class="img"/>`;
 }
